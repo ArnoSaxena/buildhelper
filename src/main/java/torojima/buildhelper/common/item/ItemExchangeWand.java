0cc7d4020d1dec26b4b979ad66712d947fa56429
Arno Saxena <al-s@gmx.de>
2016-07-21 14:17:50 +0200

    adding copyright comments on top of source files

--- src/main/java/torojima/buildhelper/common/item/ItemExchangeWand.java
+++ src/main/java/torojima/buildhelper/common/item/ItemExchangeWand.java
@@ -1,3 +1,17 @@
+// <copyright file="ItemExchangeWand.java">
+// Copyright (c) 2016 All Right Reserved, http://buildhelper.arno-saxena.de/
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// </copyright>
+// <author>Arno Saxena</author>
+// <email>al-s@gmx.de</email>
+// <date>2016-07-21</date>
+// <summary>Item class for the exchange wand item</summary>
+
 package torojima.buildhelper.common.item;
 
 import java.util.HashMap;
--- src/main/java/torojima/buildhelper/common/proxy/ProxyClient.java
+++ src/main/java/torojima/buildhelper/common/proxy/ProxyClient.java
@@ -1,3 +1,17 @@
+// <copyright file="ProxyClient.java">
+// Copyright (c) 2016 All Right Reserved, http://buildhelper.arno-saxena.de/
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// </copyright>
+// <author>Arno Saxena</author>
+// <email>al-s@gmx.de</email>
+// <date>2016-07-21</date>
+// <summary>class for client specific methods</summary>
+
 package torojima.buildhelper.common.proxy;
 
 import net.minecraft.client.Minecraft;
