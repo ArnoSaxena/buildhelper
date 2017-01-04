2fe52f8616af90ab470211f1b5196adab3f3ea2c
Arno Saxena <al-s@gmx.de>
2017-01-04 16:35:15 +0100

    move assets folder from java to resources folder

---
+++ src/main/java/torojima/buildhelper/common/item/EntityChocobo.java
@@ -0,0 +1,15 @@
+package torojima.buildhelper.common.item;
+
+import net.minecraft.entity.passive.*;
+import net.minecraft.world.World;
+
+public class EntityChocobo extends EntityHorse
+{
+
+	public EntityChocobo(World worldIn)
+	{
+		super(worldIn);
+		// TODO Auto-generated constructor stub
+	}
+
+}
